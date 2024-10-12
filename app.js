695402f812d73013e01ae346430cb8bde44a3387
taylorallen <twhitallen07@gmail.com>
2024-10-12 12:04:38 -0500

    vue basics

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "George Orwell",
+      age: 45,
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+        this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+        console.log(e, e.type)
+        if (data){
+            console.log(data);
+        }
+    },
+    handleMousemove(e) {
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+    }
+  },
+});
+
+app.mount("#app");
