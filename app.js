c7999e0161868b1515168dbf8c259ee859852e80
taylorallen <twhitallen07@gmail.com>
2024-10-12 12:19:05 -0500

    finished attribute binding; pickup at dynamic classes

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "https://4geeksacademy.com/us/index",
       showBooks: true,
-      title: "The Final Empire",
-      author: "George Orwell",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: "the philospher stone", author: "JK Rowling", img: 'assets/1.jpg' },
+        { title: "the chamber of secrets", author: "JK Rowling", img: 'assets/2.jpg'  },
+        { title: "the prisoner of azkaban", author: "Jo Rowling", img: 'assets/3.jpg'  },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
-        this.showBooks = !this.showBooks;
+      this.showBooks = !this.showBooks;
     },
-    handleEvent(e, data) {
-        console.log(e, e.type)
-        if (data){
-            console.log(data);
-        }
-    },
-    handleMousemove(e) {
-        this.x = e.offsetX;
-        this.y = e.offsetY;
-    }
   },
 });
 
